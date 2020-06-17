63dddf7fb383f39123b6c3448c407303dee55d2c
username <banawozski@gmail.com>
2020-06-17 12:25:50 +0800

    week2作業完成

--- homeworks/week2/hw1.js
+++ homeworks/week2/hw1.js
@@ -1,4 +1,14 @@
 function printStars(n) {
+	let result = []
+	for (let i= 1; i <= n ; i++){
+		if (i === n){
+			result += '*'
+		} else {
+		result += '*' + '\n'
+
+		}
+		}
+	console.log(result)
   
 }
 
--- homeworks/week2/hw5.js
+++ homeworks/week2/hw5.js
@@ -1,10 +1,27 @@
 function join(arr, concatStr) {
+	var result = [];
+	for ( var i = 0 ; i < arr.length; i++){
+		if (i == arr.length -1 && arr.length !== 1 ) {
+			result += arr[i];
+		} else {
+			result += arr[i] + concatStr
+		}
+	}
+	return result
+  
+};
+
+function repeat(str, times) {
+	var result = []
+	var Numbers = Number(times)
+	for( var i = 0 ; i < Numbers ; i ++){
+		result += str
+	}
+	return result
   
 }
 
-function repeat(str, times) {
-  
-}
+
 
 console.log(join(['a'], '!'));
 console.log(repeat('a', 5));
