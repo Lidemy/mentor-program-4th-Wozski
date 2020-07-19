f2e668cfa5d8f57274b0eacebf323ce8c5708da0
Wozski <banawozski@gmail.com>
2020-07-19 22:59:00 +0800

    hw4完成

---
+++ homeworks/week4/hw4.js
@@ -0,0 +1,19 @@
+const request = require('request');
+
+const API_ENDPOINT = {
+  url: 'https://api.twitch.tv/kraken/games/top',
+  headers: {
+    'Client-ID': 'wb6ouv69d2g65j45gudmezzey1b3lm',
+    Accept: 'application/vnd.twitchtv.v5+json',
+  },
+};
+
+function SearchTwitch() {
+  request(API_ENDPOINT, (err, res, body) => {
+    const data = JSON.parse(body);
+    for (let i = 0; i < data.top.length; i += 1) {
+      console.log('觀看人數:', data.top[i].viewers, '遊戲名稱:', data.top[i].game.name);
+    }
+  });
+}
+SearchTwitch();
